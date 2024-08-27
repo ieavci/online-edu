03b6f04d62e8e749665d5883f59889867d9a0452
ieavci <iavcci123@gmail.com>
2024-08-27 21:52:59 +0300

    Create project and add entities

---
+++ online-edu/online-edu.Entity/Entities/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_edu.Entity.Entities
+{
+	public class Course
+	{
+		public int CourseId { get; set; }
+
+		public string CourseName { get; set; }
+		public string ImageUrl { get; set; }
+		public int CategoryId { get; set; }
+
+        public decimal PriceCourse { get; set; }
+        public bool IsShown { get; set; }
+        public CourseCategory Category { get; set; }
+	}
+}
